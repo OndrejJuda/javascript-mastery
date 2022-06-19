5c07a508372d53f7911a688778638a9eecb6ba01
Ondrej Juda <ondrej.juda@networg.com>
2022-06-19 16:05:46 +0200

    01-post detail

--- 01_cms_blog/pages/index.js
+++ 01_cms_blog/pages/index.js
@@ -1,12 +1,8 @@
 import Head from 'next/head'
 import { PostCard, Categories, PostWidget } from '../components';
+import { getPosts } from '../services';
 
-const posts = [
-  { title: 'React Testing', excerpt: 'Learn React Testing', },
-  { title: 'React with Tailwind', excerpt: 'Learn React with Tailwind', },
-];
-
-const Home = () => {
+const Home = ({ posts }) => {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
@@ -16,7 +12,7 @@ const Home = () => {
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div className='lg:col-span-8 col-span-1'>
           {
-            posts.map((post) => (<PostCard post={post} key={post.title} />))
+            posts.map((post) => (<PostCard post={post.node} key={post.node.slug} />))
           }
         </div>
         <div className='lg:col-span-4 col-span-1'>
@@ -31,4 +27,14 @@ const Home = () => {
   )
 }
 
-export default Home
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+
+  return {
+    props: {
+      posts
+    }
+  };
+}
+
+export default Home;
