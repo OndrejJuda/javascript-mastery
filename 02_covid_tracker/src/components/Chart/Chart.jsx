6ebc95392ab4cba205c367a9941736e58d9f831a
Ondrej Juda <ondrej.juda@networg.com>
2022-06-25 10:42:18 +0200

    02-country picker, charts

--- 02_covid_tracker/src/components/Chart/Chart.jsx
+++ 02_covid_tracker/src/components/Chart/Chart.jsx
@@ -1,35 +1,64 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { fetchDailyData } from '../../api';
-import { Line, Bar } from 'react-chartjs-2';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
 
+} from 'chart.js';
+import { Line, Bar } from 'react-chartjs-2';
 import styles from './Chart.module.css'
 
-const Chart = () => {
-  const [dailyData, setDailyData] = useState({});
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+
+const Chart = ({ data: { confirmed, recovered, deaths }, country }) => {
+  const [dailyData, setDailyData] = useState([]);
+
+  const lineRef = useRef();
 
   useEffect(() => {
     const fetchData = async () => {
-      const dailyData = await fetchDailyData();
-      console.log(dailyData)
-      setDailyData(dailyData);
+      setDailyData(await fetchDailyData());
     };
     fetchData();
   }, []);
 
+  useEffect(() => {
+    lineRef.current && lineRef.current.update();
+  }, [dailyData]);
+
   const lineChart = (
-    dailyData[0]
+    dailyData.length
       ? <Line
         data={{
-          labels: dailyData.map(({ reportDate }) => reportDate),
+          labels: dailyData.map(({ date }) => new Date(date).toDateString()),
           datasets: [
             {
-              data: dailyData.map(({ totalConfirmed }) => totalConfirmed),
+              id: 1,
+              data: dailyData.map(({ confirmed }) => confirmed),
               label: 'Infected',
               borderColor: '#90f',
               fill: true
             },
             {
-              data: dailyData.map(({ deaths: { total } }) => total),
+              id: 2,
+              data: dailyData.map(({ deaths }) => deaths),
               label: 'Deaths',
               borderColor: 'red',
               backgroundColor: 'rgba(255, 0, 0, 0.5)',
@@ -41,9 +70,34 @@ const Chart = () => {
       : null
   );
 
+  const barChart = (
+    confirmed ? (
+      <Bar
+        data={{
+          labels: ['Infected', 'Recovered', 'Deaths'],
+          datasets: [
+            {
+              label: 'People',
+              backgroundColor: [
+                'rgba(153, 0, 255, 0.5)',
+                'rgba(0, 255, 0, 0.5)',
+                'rgba(255, 0, 0, 0.5)',
+              ],
+              data: [confirmed.value, recovered.value, deaths.value],
+            }
+          ],
+        }}
+        options={{
+          legend: { display: false },
+          title: { display: true, text: `Current state is ${country}` }
+        }}
+      />
+    ) : null
+  );
+
   return (
     <div className={styles.container}>
-      {lineChart}
+      {country ? barChart : lineChart}
     </div>
   );
 };
--- 02_covid_tracker/src/components/CountryPicker/CountryPicker.jsx
+++ 02_covid_tracker/src/components/CountryPicker/CountryPicker.jsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { NativeSelect, FormControl } from '@material-ui/core';
+
+import { fetchCountries } from '../../api';
 
 import styles from './CountryPicker.module.css'
 
-const CountryPicker = () => {
+const CountryPicker = ({ handleCountryChange }) => {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const fetchAPI = async () => {
+      const countriess = await fetchCountries();
+      setCountries(countriess);
+    };
+    fetchAPI();
+  }, []);
+
   return (
-    <div>CountryPicker</div>
+    <FormControl className={styles.formControl}>
+      <NativeSelect defaultValue={''} onChange={(event) => handleCountryChange(event.target.value)}>
+        <option value=''>Global</option>
+        {
+
+          countries.map((country, i) => (
+            <option key={i} value={country}>{country}</option>
+          ))
+        }
+      </NativeSelect>
+    </FormControl>
   );
 };
 
