6d2c95f466cf2d276fea7b9102ffba99f4c7a486
Ondrej Juda <ondrej.juda@networg.com>
2022-07-30 06:14:23 +0200

    05 - what is gpt3 section

--- 05_gbt3_openai_landingpage/src/components/feature/Feature.jsx
+++ 05_gbt3_openai_landingpage/src/components/feature/Feature.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import './Feature.css';
 
-const Feature = () => {
+const Feature = ({ title, text, direction, fullWidth }) => {
+  console.log(title, fullWidth)
   return (
-    <div>
-      Feature
+    <div className="gpt3__features-container__feature" style={{
+      flexDirection: direction === 'row' ? 'row' : 'column',
+      width: fullWidth ? '100%' : 'auto'
+    }}>
+      <div className="gpt3__features-container__title">
+        <div />
+        <h1>{title}</h1>
+      </div>
+      <div className="gpt3__features-container_feature-text">
+        <p>{text}</p>
+      </div>
     </div>
   );
 };
