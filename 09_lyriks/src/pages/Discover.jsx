e8f5ade675aaaf9e5ab3c0550cf8cea21e5a2eba
Ondrej Juda <ondrej.juda@networg.com>
2022-09-30 07:04:11 +0200

    09 - discover page

--- 09_lyriks/src/pages/Discover.jsx
+++ 09_lyriks/src/pages/Discover.jsx
@@ -1,8 +1,11 @@
+import { useDispatch, useSelector } from 'react-redux';
 import { Error, Loader, SongCard } from '../components';
 import { genres } from '../assets/constants';
 import { useGetTopChartsQuery } from '../redux/services/shazamCore';
 
 const Discover = () => {
+  const dispatch = useDispatch();
+  const { activeSong, isPlaying } = useSelector((selector) => selector.player);
   const { data, isFetching, error } = useGetTopChartsQuery();
   const genreTitle = 'Pop';
 
@@ -40,6 +43,9 @@ const Discover = () => {
             <SongCard
               key={song.key}
               song={song}
+              activeSong={activeSong}
+              isPlaying={isPlaying}
+              data={data}
               i={i}
             />
           ))
