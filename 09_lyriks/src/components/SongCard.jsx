a8906f07c1e3cedec4903a963d224c08b160dd7c
Ondrej Juda <ondrej.juda@networg.com>
2022-09-30 07:11:21 +0200

    09 - song card adjustment

--- 09_lyriks/src/components/SongCard.jsx
+++ 09_lyriks/src/components/SongCard.jsx
@@ -18,8 +18,9 @@ const SongCard = ({ song, i, data, isPlaying, activeSong }) => {
 
   return (
     <div className='flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer'>
-      <div className='relative w-full h-56 group'>
-        <div className={`absolute inset-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex ${activeSong?.title === song?.title ? 'flex bg-black bg-opacity-70' : 'hidden'}`}>
+      <div className='relative w-full h-[218px] group rounded-md'>
+        <img src={song.images?.coverart} alt='song img' className='rounded-md' />
+        <div className={`absolute rounded-md inset-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex ${activeSong?.title === song?.title ? 'flex bg-black bg-opacity-70' : 'hidden'}`}>
           <PlayPause
             song={song}
             activeSong={activeSong}
@@ -28,7 +29,6 @@ const SongCard = ({ song, i, data, isPlaying, activeSong }) => {
             handlePlay={handlePlayClick}
           />
         </div>
-        <img src={song.images?.coverart} alt='song img' />
       </div>
       <div className='mt-4 flex flex-col'>
         <p className='font-semibold text-lg text-white truncate'>
