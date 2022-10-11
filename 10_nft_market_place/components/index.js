995edb6d44d65d24719c89a38d877557701143b9
Ondrej Juda <ondrej.juda@networg.com>
2022-10-11 20:59:26 +0200

    10 - nft card component

---
+++ 10_nft_market_place/components/index.js
@@ -0,0 +1,19 @@
+import HomeHeader from './HomeHeader';
+import FocusedStatusBar from './FocusedStatusBar';
+import NFTCard from './NFTCard';
+import { CircleButton, RectButton } from './Button';
+import { EndDate, EthPrice, ImageCmp, NFTTitle, People, SubInfo } from './SubInfo';
+
+export {
+  HomeHeader,
+  FocusedStatusBar,
+  NFTCard,
+  CircleButton,
+  RectButton,
+  SubInfo,
+  EndDate,
+  EthPrice,
+  ImageCmp,
+  NFTTitle,
+  People,
+}
