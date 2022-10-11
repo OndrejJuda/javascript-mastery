0b23ed2b09372edd27875019788cec880f0885ec
Ondrej Juda <ondrej.juda@networg.com>
2022-10-11 07:34:46 +0200

    10 - init project

---
+++ 10_nft_market_place/constants/index.js
@@ -0,0 +1,5 @@
+import assets from "../constants/assets";
+import { COLORS, SHADOWS, SIZES, FONTS } from "./theme";
+import { NFTData } from "./dummy";
+
+export { assets, COLORS, SHADOWS, SIZES, FONTS, NFTData };
