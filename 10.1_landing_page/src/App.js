2ce767c4f3d2af02df0de03bd49933bf40687ec9
Ondrej Juda <ondrej.juda@networg.com>
2022-10-17 06:35:37 +0200

    10.1 - landing page

--- 10.1_landing_page/src/App.js
+++ 10.1_landing_page/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import assets from './assets';
+import { Features, Download, SectionWrapper } from './components';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <SectionWrapper
+        title='Your own store of Nifty NFTs. Start Selling & Growing'
+        description='Buy, store, collect NFTs, exchange & earn crypto. Join 25+ million people using ProNef Marketplace.'
+        showBtn
+        mockupImg={assets.homeHero}
+        banner='banner'
+      />
+    </>
   );
-}
+};
 
 export default App;
